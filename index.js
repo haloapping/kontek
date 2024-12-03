da5e3e1ea99fb0dc001abe941cbe5f93f52ee704
haloapping <alfiyanto.kondolele@gmail.com>
2024-12-03 07:21:24 +0700

    add array of object contact and show using console.log

---
+++ index.js
@@ -0,0 +1,81 @@
+let contacts = [
+  {
+    id: 1,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+  {
+    id: 2,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+  {
+    id: 3,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+  {
+    id: 4,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+  {
+    id: 5,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+  {
+    id: 5,
+    fullName: "John Doe",
+    phone: "081234567891",
+    email: "doe@jamil.com",
+    company: "Toko Pake Edi",
+    jobRole: "CRUD Engineer",
+    address: "Jalan Jalan",
+    birthday: "11-12-2012",
+    notes: "notes"
+  },
+];
+
+console.log("KonTek List:");
+for (let i = 0; i < contacts.length; i++) {
+  console.log(`Id        : ${contacts[i].id}`);
+  console.log(`Full Name : ${contacts[i].fullName}`);
+  console.log(`Phone     : ${contacts[i].phone}`);
+  console.log(`Email     : ${contacts[i].email}`);
+  console.log(`Company   : ${contacts[i].company}`);
+  console.log(`Job Role  : ${contacts[i].jobRole}`);
+  console.log(`Birthday  : ${contacts[i].birthday}`);
+  console.log(`Notes     : ${contacts[i].notes}`);
+  console.log("========================================");
+}
