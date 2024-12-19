d05460ad281dba5b2a8d95e9a100cfab32dd818a
haloapping <alfiyanto.kondolele@gmail.com>
2024-12-20 06:44:49 +0700

    use DOM to manipulation html element

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ let contacts = [
     fullName: "Appang",
     phone: "081234567891",
     email: "appang@jamil.com",
-    company: "Toko Pak Edi",
+    company: "Buka Toko",
     jobTitle: "CRUD Engineer",
     address: "Jalan Jalan",
     birthdate: new Date("1998-01-11"),
     note: "note",
     label: "Assistant",
-    isFavourite: true,
+    isFavorited: true,
     createdAt: new Date(),
     updatedAt: null,
     deletedAt: null,
@@ -26,54 +26,199 @@ let contacts = [
     birthdate: new Date("1999-02-15"),
     note: "note",
     label: "Partner",
-    isFavourite: false,
+    isFavorited: false,
     createdAt: new Date(),
     updatedAt: null,
     deletedAt: null,
   },
 ];
 
+function saveContacts(contacts) {
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+}
+
+function loadContacts() {
+  return JSON.parse(localStorage.getItem("contacts"));
+}
+
+saveContacts(contacts);
+let localStorageContacts = loadContacts(contacts);
+
 function showAllContacts(contacts) {
   if (contacts.length === 0) {
-    console.log("Contact is empty.");
+    const pElement = document.createElement("p");
+    pElement.innerText = "Contact is empty.";
+
+    const mainElement = document.getElementById("main");
+    mainElement.append(pElement);
   } else {
-    for (let i = 0; i < contacts.length; i++) {
-      if (contacts[i].deletedAt === null) {
-        console.log(`Id        : ${contacts[i].ID}`);
-        console.log(`Full Name : ${contacts[i].fullName}`);
-        console.log(`Phone     : ${contacts[i].phone}`);
-        console.log(`Email     : ${contacts[i].email}`);
-        console.log(`Company   : ${contacts[i].company}`);
-        console.log(`Job Title : ${contacts[i].jobTitle}`);
-        console.log(`Birthdate : ${contacts[i].birthdate.toLocaleDateString()}`);
-        console.log(`Note      : ${contacts[i].note}`);
-        console.log(`Label     : ${contacts[i].label}`);
-        console.log(`Favourite : ${contacts[i].isFavourite ? "✅️" : "❌️"}`);
-        console.log("-------------------------------------------");
+    const table = `
+      <table>
+        <caption>
+          List Contacts
+        </caption>
+        <thead>
+          <tr>
+            <td>Full Name</td>
+            <td>Phone</td>
+            <td>Email</td>
+            <td>Company</td>
+            <td>Job Title</td>
+            <td>Birthdate</td>
+            <td>Note</td>
+            <td>Label</td>
+            <td>Favorited</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+
+        <tbody id="table-body"></tbody>
+      </table>
+    `;
+
+    const mainElement = document.getElementById("main");
+    mainElement.innerHTML += table;
+
+    const tbodyElement = document.getElementById("table-body");
+    contacts.forEach((contact) => {
+      if (contact.deletedAt === null) {
+        const trElement = document.createElement("tr");
+
+        const tdFullNameElement = document.createElement("td");
+        tdFullNameElement.textContent = contact.fullName;
+
+        const tdPhoneElement = document.createElement("td");
+        tdPhoneElement.textContent = contact.phone;
+
+        const tdEmailElement = document.createElement("td");
+        tdEmailElement.textContent = contact.email;
+
+        const tdCompanyElement = document.createElement("td");
+        tdCompanyElement.textContent = contact.company;
+
+        const tdJobTitleElement = document.createElement("td");
+        tdJobTitleElement.textContent = contact.jobTitle;
+
+        const tdBirthdateElement = document.createElement("td");
+        tdBirthdateElement.textContent = contact.birthdate;
+
+        const tdNoteElement = document.createElement("td");
+        tdNoteElement.textContent = contact.note;
+
+        const tdLabelElement = document.createElement("td");
+        tdLabelElement.textContent = contact.label;
+
+        const tdFavoritedElement = document.createElement("td");
+        tdFavoritedElement.textContent = contact.isFavorited ? "✅️" : "❌️";
+
+        const detailButtonElement = document.createElement("button");
+        detailButtonElement.textContent = "Detail";
+        detailButtonElement.type = "submit";
+        detailButtonElement.addEventListener("click", () => {
+          showDetailContactByID(contacts, contact.ID);
+        });
+
+        const editButtonElement = document.createElement("button");
+        editButtonElement.textContent = "Edit";
+        editButtonElement.type = "submit";
+        editButtonElement.addEventListener("click", () => {
+          updateContactByID(contacts, contact.ID);
+        });
+
+        const deleteButtonElement = document.createElement("button");
+        deleteButtonElement.textContent = "Delete";
+        deleteButtonElement.type = "submit";
+        deleteButtonElement.addEventListener("click", () => {
+          deleteContactById(contacts, contact.ID);
+        });
+
+        const tdActionElement = document.createElement("td");
+        tdActionElement.append(detailButtonElement, editButtonElement, deleteButtonElement);
+
+        trElement.append(
+          tdFullNameElement,
+          tdPhoneElement,
+          tdEmailElement,
+          tdCompanyElement,
+          tdJobTitleElement,
+          tdBirthdateElement,
+          tdNoteElement,
+          tdLabelElement,
+          tdFavoritedElement,
+          tdActionElement
+        );
+
+        tbodyElement.appendChild(trElement);
       }
-    }
+    });
   }
+}
+
+function showDetailContactByID(contacts, ID) {
+  window.location.href = window.origin + "/detail-contact.html";
+
+  const contact = contacts.find((contact) => contact.ID === ID);
+
+  const contactDetailUlElement = document.getElementById("contact-detail");
+
+  const fullNameLiElement = document.createElement("li");
+  fullNameLiElement.textContent = contact.fullName;
+
+  const phoneLiElement = document.createElement("li");
+  phoneLiElement.textContent = contact.phone;
+
+  const emailLiElement = document.createElement("li");
+  emailLiElement.textContent = contact.email;
+
+  const companyLiElement = document.createElement("li");
+  companyLiElement.textContent = contact.company;
+
+  const jobTitleLiElement = document.createElement("li");
+  jobTitleLiElement.textContent = contact.jobTitle;
+
+  const addressLiElement = document.createElement("li");
+  addressLiElement.textContent = contact.address;
+
+  const birthdayLiElement = document.createElement("li");
+  birthdayLiElement.textContent = contact.birthdate;
+
+  const noteLiElement = document.createElement("li");
+  noteLiElement.textContent = contact.note;
+
+  const labelLiElement = document.createElement("li");
+  labelLiElement.textContent = contact.label;
+
+  const favoritedLiElement = document.createElement("li");
+  favoritedLiElement.textContent = contact.isFavorited ? "✅️" : "❌️";
+
+  const createdAtLiElement = document.createElement("li");
+  createdAtLiElement.textContent = contact.createdAt;
+
+  const updatedAtLiElement = document.createElement("li");
+  updatedAtLiElement.textContent = contact.updatedAt;
+
+  const deletedAtLiElement = document.createElement("li");
+  deletedAtLiElement.textContent = contact.deletedAt;
+
+  contactDetailUlElement.append(
+    fullNameLiElement,
+    phoneLiElement,
+    emailLiElement,
+    companyLiElement,
+    jobTitleLiElement,
+    addressLiElement,
+    birthdayLiElement,
+    noteLiElement,
+    labelLiElement,
+    favoritedLiElement,
+    createdAtLiElement,
+    updatedAtLiElement,
+    updatedAtLiElement
+  );
 }
 
 function getContactByID(contacts, ID) {
   return contacts.find((contact) => contact.ID === ID && contact.deletedAt === null);
-}
-
-function showDetailContactByID(contact) {
-  console.log(`ID         : ${contact.ID}`);
-  console.log(`Full Name  : ${contact.fullName}`);
-  console.log(`Phone      : ${contact.phone}`);
-  console.log(`Email      : ${contact.email}`);
-  console.log(`Company    : ${contact.company}`);
-  console.log(`Job Title  : ${contact.jobTitle}`);
-  console.log(`Address    : ${contact.address}`);
-  console.log(`Birthdate  : ${contact.birthdate.toLocaleDateString()}`);
-  console.log(`Note       : ${contact.note}`);
-  console.log(`Label      : ${contact.label}`);
-  console.log(`Favourite  : ${contact.isFavourite ? "✅️" : "❌️"}`);
-  console.log(`Created At : ${contact.createdAt}`);
-  console.log(`Updated At : ${contact.updatedAt}`);
-  console.log(`Deleted At : ${contact.deletedAt}`);
 }
 
 function searchContacts(contacts, keyword) {
@@ -85,7 +230,7 @@ function searchContacts(contacts, keyword) {
       contact.company.includes(keyword) ||
       contact.jobTitle.includes(keyword) ||
       contact.address.includes(keyword) ||
-      contact.birthdate.toLocaleDateString().includes(keyword) ||
+      contact.birthdate.includes(keyword) ||
       contact.note.includes(keyword) ||
       contact.label.includes(keyword)
   );
@@ -93,57 +238,50 @@ function searchContacts(contacts, keyword) {
   return foundContacts;
 }
 
-function addContact(contacts) {
-  const ID = contacts[contacts.length - 1].ID + 1;
-  const fullName = prompt("Full Name    :");
-  const phone = prompt("Phone        :");
-  const email = prompt("Email        :");
-  const company = prompt("Company      :");
-  const jobTitle = prompt("Job Title    :");
-  const address = prompt("Address      :");
-  const birthdate = prompt("Birthdate    :");
-  const note = prompt("Note         :");
-  const label = prompt("Label        :");
-  const isFavourite = prompt("Is Favourite :");
-
+function addContact(contacts, newContact) {
   contacts.push({
-    ID: ID,
-    fullName: fullName,
-    phone: phone,
-    email: email,
-    company: company,
-    jobTitle: jobTitle,
-    address: address,
-    birthdate: new Date(birthdate),
-    note: note,
-    label: label,
-    isFavourite: isFavourite,
-    createdAt: new Date(),
+    ID: contacts[contacts.length - 1].ID + 1,
+    fullName: newContact.fullName,
+    phone: newContact.phone,
+    email: newContact.email,
+    company: newContact.company,
+    jobTitle: newContact.jobTitle,
+    address: newContact.address,
+    birthdate: newContact.birthdate,
+    note: newContact.note,
+    label: newContact.label,
+    isFavorited: newContact.isFavorited,
+    createdAt: newContact.createdAt,
     updatedAt: null,
     deletedAt: null,
   });
 
+  saveContacts(contacts);
+
   console.log("Contact is added :)");
 }
 
-function updateContactByID(contacts, ID) {
+function updateContactByID(contacts, ID, updateContact) {
+  // find by ID
+  // replace object by id with map function
   let index;
   for (index = 0; index < contacts.length; index++) {
     if (contacts[index].ID === ID) {
-      contacts[index].fullName = prompt(`Full Name (${contacts[index].fullName}):`) || contacts[index].fullName;
-      contacts[index].phone = prompt(`Phone (${contacts[index].phone}):`) || contacts[index].phone;
-      contacts[index].email = prompt(`Email (${contacts[index].email}):`) || contacts[index].email;
-      contacts[index].company = prompt(`Company (${contacts[index].company}):`) || contacts[index].company;
-      contacts[index].jobTitle = prompt(`Job Title (${contacts[index].jobTitle}):`) || contacts[index].jobTitle;
-      contacts[index].address = prompt(`Address (${contacts[index].address}):`) || contacts[index].address;
-      contacts[index].birthdate = prompt(`Birthdate (${contacts[index].birthdate}):`) || contacts[index].birthdate;
-      contacts[index].note = prompt(`Note (${contacts[index].note}): `) || contacts[index].note;
-      contacts[index].label = prompt(`Label (${contacts[index].label}):`) || contacts[index].label;
-      const isFavourite = prompt(`Is favourite (${contacts[index].isFavourite}):`);
-      if (isFavourite !== null) {
-        contacts[index].isFavourite = isFavourite === "true";
+      contacts[index].fullName = updateContact.fullName || contacts[index].fullName;
+      contacts[index].phone = updateContact.phone || contacts[index].phone;
+      contacts[index].email = updateContact.email || contacts[index].email;
+      contacts[index].company = updateContact.company || contacts[index].company;
+      contacts[index].jobTitle = updateContact.jobTitle || contacts[index].jobTitle;
+      contacts[index].address = updateContact.address || contacts[index].address;
+      contacts[index].birthdate = updateContact.birthdate || contacts[index].birthdate;
+      contacts[index].note = updateContact.note || contacts[index].note;
+      contacts[index].label = updateContact.label || contacts[index].label;
+      const isFavorited = updateContact.isFavorited;
+      if (isFavorited !== null) {
+        contacts[index].isFavorited = isFavorited === true;
       }
       contacts[index].updatedAt = new Date();
+      saveContacts(contacts);
       console.log(`Contact with ID ${ID} is updated :)`);
       break;
     }
@@ -155,80 +293,47 @@ function updateContactByID(contacts, ID) {
 }
 
 function deleteContactById(contacts, ID) {
-  const indexContact = contacts.findIndex((contact) => contact.ID === ID && contact.deletedAt === null);
-
-  if (indexContact === -1) {
-    console.log(`Contact with ID ${ID} not found`);
-  } else {
+  const isDeleted = confirm("Are you sure delete this data?");
+  if (isDeleted) {
+    const indexContact = contacts.findIndex((contact) => contact.ID === ID && contact.deletedAt === null);
     contacts[indexContact].deletedAt = new Date();
+    saveContacts(contacts);
     console.log(`Contact with ID ${ID} is deleted :)`);
   }
 }
 
-while (true) {
-  console.log(` ___   _  _______  __    _  _______  _______  ___   _ 
-|   | | ||       ||  |  | ||       ||       ||   | | |
-|   |_| ||   _   ||   |_| ||_     _||    ___||   |_| |
-|      _||  | |  ||       |  |   |  |   |___ |      _|
-|     |_ |  |_|  ||  _    |  |   |  |    ___||     |_ 
-|    _  ||       || | |   |  |   |  |   |___ |    _  |
-|___| |_||_______||_|  |__|  |___|  |_______||___| |_|
-`);
-  console.log("List of Menu:");
-  console.log("[1] Show All Contacts");
-  console.log("[2] Detail Contact");
-  console.log("[3] Search Contact");
-  console.log("[4] Add Contact");
-  console.log("[5] Update Contact");
-  console.log("[6] Delete Contact");
-  console.log("[7] Exit");
-  console.log("===========================================\n");
-  let menuOption = prompt("Choose menu (1/2/3/4/5/6/7):");
+// Add Contact
+console.log("Add Contact");
+const newContact = {
+  fullName: "Appeng",
+  phone: "081234567891",
+  email: "apping@jamil.com",
+  company: "Toko Pak Edi",
+  jobTitle: "CRUD Engineer",
+  address: "Jalan Jalan",
+  birthdate: new Date("1999-02-15"),
+  note: "note",
+  label: "Partner",
+  isFavorited: true,
+  createdAt: new Date(),
+  updatedAt: null,
+  deletedAt: null,
+};
+addContact(localStorageContacts, newContact);
 
-  if (menuOption === "1") {
-    console.log("SHOW ALL CONTACTS\n");
-    showAllContacts(contacts);
-    console.log("===========================================\n");
-  } else if (menuOption === "2") {
-    console.log("DETAIL CONTACT BY ID\n");
-    const ID = parseInt(prompt("Enter ID Contact:"));
-    const contactByID = getContactByID(contacts, ID);
-    if (contactByID === undefined) {
-      console.log("Contact not found");
-    } else {
-      showDetailContactByID(contactByID);
-    }
-    console.log("===========================================\n");
-  } else if (menuOption === "3") {
-    console.log("SEARCH CONTACT\n");
-    const keyword = prompt("Enter keyword:");
-    const foundContacts = searchContacts(contacts, keyword);
-    if (foundContacts.length === 0) {
-      console.log("Contact not found");
-    } else {
-      showAllContacts(foundContacts);
-    }
-    console.log("===========================================\n");
-  } else if (menuOption === "4") {
-    console.log("ADD CONTACT\n");
-    addContact(contacts);
-  } else if (menuOption === "5") {
-    console.log("UPDATE CONTACT\n");
-    const ID = parseInt(prompt("Enter ID Contact:"));
-    updateContactByID(contacts, ID);
-    console.log("===========================================\n");
-  } else if (menuOption === "6") {
-    console.log("DELETE CONTACT");
-    const ID = parseInt(prompt("Enter ID Contact:"));
-    deleteContactById(contacts, ID);
-    console.log("===========================================\n");
-  } else if (menuOption === "7") {
-    console.log("Bye!");
-    break;
-  } else {
-    console.log(`Menu ${menuOption} is not available. Use menu 1/2/3/4/5/6/7.\n`);
-    console.log("===========================================\n");
-  }
+// Update contact
+const updateContact = {
+  fullName: "Appong",
+  phone: null,
+  email: null,
+  company: null,
+  jobTitle: null,
+  address: null,
+  birthdate: null,
+  note: null,
+  label: null,
+  isFavorited: true,
+};
+updateContactByID(localStorageContacts, 3, updateContact);
 
-  prompt("Enter to continue...");
-}
+showAllContacts(localStorageContacts);
