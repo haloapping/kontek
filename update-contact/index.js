f63bb65fda729e2da78240c428014566578244dd
M Haidar Hanif <git@mhaidarhanif.com>
2024-12-23 21:25:32 +0700

    fix: update contact

--- update-contact/index.js
+++ update-contact/index.js
@@ -33,7 +33,7 @@ note.setAttribute("value", contactByID.note);
 note.textContent = contactByID.note;
 
 const label = document.getElementById("label");
-console.log(label.options[1].selected);
+
 if (contactByID.label === "Partner") {
   label.options[1].selected = true;
 } else if (contactByID.label === "Manager") {
@@ -51,30 +51,37 @@ const isFavorited = document.getElementById("is-favorited");
 const createdAt = document.getElementById("created-at");
 createdAt.setAttribute("value", contactByID.createdAt);
 
-document
-  .getElementById("update-contact-from")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+const updateContactFormElement = document.getElementById("update-contact-form");
 
-    const updateContactFormData = new FormData(this);
-    const updatedContact = {
-      ID: contactID,
-      fullName: updateContactFormData.get("full-name"),
-      phone: updateContactFormData.get("phone"),
-      email: updateContactFormData.get("email"),
-      company: updateContactFormData.get("company"),
-      jobTitle: updateContactFormData.get("job-title"),
-      address: updateContactFormData.get("address"),
-      birthdate: updateContactFormData.get("birthdate"),
-      note: updateContactFormData.get("notes"),
-      label: updateContactFormData.get("label"),
-      isFavorited: updateContactFormData.get("is-favorited"),
-      createdAt: updateContactFormData.get("created-at"),
-      updatedAt: new Date(),
-    };
+updateContactFormElement.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    localStorageContacts[contactByID] = updatedContact;
-    saveContacts(localStorageContacts);
+  const updateContactFormData = new FormData(this);
+  const updatedContact = {
+    ID: contactID,
+    fullName: updateContactFormData.get("full-name"),
+    phone: updateContactFormData.get("phone"),
+    email: updateContactFormData.get("email"),
+    company: updateContactFormData.get("company"),
+    jobTitle: updateContactFormData.get("job-title"),
+    address: updateContactFormData.get("address"),
+    birthdate: updateContactFormData.get("birthdate"),
+    note: updateContactFormData.get("notes"),
+    label: updateContactFormData.get("label"),
+    isFavorited: updateContactFormData.get("is-favorited"),
+    createdAt: updateContactFormData.get("created-at"),
+    updatedAt: new Date(),
+  };
 
-    window.location.href = "/";
+  // Update data with map
+  const updatedContacts = localStorageContacts.map((contact) => {
+    if (contact.ID === contactID) {
+      return updatedContact;
+    }
+    return contact;
   });
+
+  saveContacts(updatedContacts);
+
+  window.location.href = "/";
+});
